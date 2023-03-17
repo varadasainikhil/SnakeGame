969475a669df857d163b1ef9dda55c798c36d548
Sai Nikhil Varada <varadasainikhil@gmail.com>
2023-03-17 19:05:26 +0530

    Added the food class

---
+++ food.py
@@ -0,0 +1,16 @@
+from turtle import Turtle
+import random
+
+class Food(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.penup()
+        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
+        self.color("blue")
+        self.speed("fastest")
+
+    def go_to_new_location(self):
+        random_x = random.randint(-270, 270)
+        random_y = random.randint(-270, 270)
+        self.goto(random_x, random_y)
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 import time
 from snake import Snake
+from food import Food
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -8,6 +9,8 @@ screen.bgcolor("black")
 screen.title("Snake Game")
 screen.tracer(0)
 snake = Snake()
+food = Food()
+food.go_to_new_location()
 game_on = True
 
 screen.listen()
@@ -20,5 +23,9 @@ while game_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
+    # Detect collision with the food
+    if food.distance(snake.head) < 15:
+        food.go_to_new_location()
+
 
 screen.exitonclick()
